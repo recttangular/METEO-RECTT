e309c733481d13c4b0966f71e65d83fc07a1c135
recttangular <sudrilhafizamin@gmail.com>
2025-11-27 23:58:24 +0700

    ganti kota deh

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const cities = [
     { id: 1, name: "Jakarta", lat: -6.2088, lon: 106.8456 },
     { id: 2, name: "Makassar", lat: -5.1477, lon: 119.4327 },
-    { id: 3, name: "Bandung", lat: -6.9175, lon: 107.6191 },
+    { id: 3, name: "Padang", lat: -0.56, lon: 100.22 },
     { id: 4, name: "Jayapura", lat: -2.5337, lon: 140.7181 },
     { id: 5, name: "Tokyo", lat: 35.6895, lon: 139.6917 }
 ];
